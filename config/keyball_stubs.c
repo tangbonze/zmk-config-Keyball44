917e40ccc87f7dd70dc2bc554ed3be40c5c411e8
tangbonze <fengk@outlook.com>
2026-07-01 11:59:18 +0800

    fix(peripheral): configure config directory as Zephyr module to compile stubs on peripheral side

--- config/keyball_stubs.c
+++ config/keyball_stubs.c
@@ -1,13 +1,12 @@
 #include <zephyr/kernel.h>
-#include <zmk/keymap.h>
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 
-int zmk_keymap_layer_activate(zmk_keymap_layer_id_t layer) {
+int zmk_keymap_layer_activate(uint8_t layer) {
     return 0;
 }
 
-int zmk_keymap_layer_deactivate(zmk_keymap_layer_id_t layer) {
+int zmk_keymap_layer_deactivate(uint8_t layer) {
     return 0;
 }
 
